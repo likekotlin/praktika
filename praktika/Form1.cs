d757573297cf7ba3db4e3fdd4b96e84b0fc145b9
Михаил Коченов <kaganmixa203@icloud.com>
2021-12-03 21:46:34 +0300

    Сделал асинхронный запуск  и добавил обработчики кнопок

--- praktika/Form1.cs
+++ praktika/Form1.cs
@@ -12,9 +12,47 @@ namespace praktika
 {
     public partial class Form1 : Form
     {
+        private delegate int AsyncSumm(int a, int b);
+        private int Summ(int a, int b)
+        {
+            System.Threading.Thread.Sleep(9000);
+            return a + b;
+        }
+        private void CallBackMethod(IAsyncResult ar)
+        {
+            string str;
+            AsyncSumm summdelegate = (AsyncSumm)ar.AsyncState;
+            str = string.Format("Сумма введенных чисел равна {0}", summdelegate.EndInvoke(ar));
+            MessageBox.Show(str, "Результат операции");
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRun_Click_1(object sender, EventArgs e)
+        {
+            int a, b;
+            try
+            {
+                //Преобрахование типов данных
+                a = Int32.Parse(txtA.Text);
+                b = Int32.Parse(txbB.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("При выполнентт преобразования типов возникла ошибка");
+                txtA.Text = txbB.Text = " ";
+                return;
+            }
+            AsyncSumm summdelegate = new AsyncSumm(Summ);
+            AsyncCallback cd = new AsyncCallback(CallBackMethod);
+            summdelegate.BeginInvoke(a, b, cd, summdelegate);
+        }
+
+        private void btnWork_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работа идет полным ходом");
         }
     }
 }
