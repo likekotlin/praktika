ffc95968bb4acfa5a85a6548e702ca9ee0bef5ef
Михаил Коченов <kaganmixa203@icloud.com>
2021-12-04 16:52:52 +0300

    Добавил всплывающие подсказки tooltip на форму

--- praktika/Form1.Designer.cs
+++ praktika/Form1.Designer.cs
@@ -30,12 +30,98 @@ namespace praktika
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(57, 104);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Сумма";
+            this.toolTip1.SetToolTip(this.btnRun, "Sum");
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(237, 154);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 23);
+            this.btnWork.TabIndex = 1;
+            this.btnWork.Text = "Работа";
+            this.toolTip1.SetToolTip(this.btnWork, "Start work");
+            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(139, 36);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(100, 22);
+            this.txbA.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txbA, "For input integer A");
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(356, 39);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 22);
+            this.txbB.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.txbB, "For input integer B");
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(24, 39);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(86, 17);
+            this.lblA.TabIndex = 4;
+            this.lblA.Text = "Значение А";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(264, 36);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(86, 17);
+            this.lblB.TabIndex = 5;
+            this.lblB.Text = "Значение В";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(478, 196);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
