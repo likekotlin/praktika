c703d595c909faa6b583ce93dac20b909848b00b
Михаил Коченов <kaganmixa203@icloud.com>
2021-12-06 12:11:32 +0300

    Создал связаннуй с данными форму в мастере ИД

--- praktika/Form1.cs
+++ praktika/Form1.cs
@@ -16,5 +16,22 @@ namespace praktika
         {
             InitializeComponent();
         }
+
+        private void сотрудникиBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.сотрудникиBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.konfetnaya_fabrikaDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "konfetnaya_fabrikaDataSet.Товары". При необходимости она может быть перемещена или удалена.
+            this.товарыTableAdapter.Fill(this.konfetnaya_fabrikaDataSet.Товары);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "konfetnaya_fabrikaDataSet.Сотрудники". При необходимости она может быть перемещена или удалена.
+            this.сотрудникиTableAdapter.Fill(this.konfetnaya_fabrikaDataSet.Сотрудники);
+
+        }
     }
 }
